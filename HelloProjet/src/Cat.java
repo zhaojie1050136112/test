e5838a3a6b39860358be777eb65402251615453f
zhaojie1050136112 <52374576+zhaojie1050136112@users.noreply.github.com>
2019-08-17 11:42:17 +0800

    Update Cat.java
    用命令的方式拉取代码

--- HelloProjet/src/Cat.java
+++ HelloProjet/src/Cat.java
@@ -2,5 +2,6 @@ public class Cat {
     public static void main(String[] args) {
         System.out.println("添加了一个类");
         System.out.println("猫名称叫汤姆");
+        System.out.println("用命令的方式拉取代码");
     }
 }
